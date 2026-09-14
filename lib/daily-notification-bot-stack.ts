b25c2212efe695c45f73a10b8301b36622040b63
Alexey Sergeev <xengawe@gmail.com>
2026-09-14 16:19:49 -0700

    fix cron hourly job

--- lib/daily-notification-bot-stack.ts
+++ lib/daily-notification-bot-stack.ts
@@ -84,7 +84,7 @@ export class DailyNotificationBotStack extends cdk.Stack {
     new events.Rule(this, "HourlyCronSchedule", {
       schedule: events.Schedule.cron({
         minute: "1",
-        hour: "1-13",
+        hour: "13-23,0-1",
         weekDay: "*",
       }),
       targets: [new eventTargets.LambdaFunction(scheduledFn)],
