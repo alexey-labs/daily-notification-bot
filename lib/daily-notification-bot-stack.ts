22f48d73be8ef2f7dc1803c114b5ec60566c7d05
Alexey Sergeev <xengawe@gmail.com>
2026-09-14 11:44:17 -0700

    fix time

--- lib/daily-notification-bot-stack.ts
+++ lib/daily-notification-bot-stack.ts
@@ -84,7 +84,7 @@ export class DailyNotificationBotStack extends cdk.Stack {
     new events.Rule(this, "HourlyCronSchedule", {
       schedule: events.Schedule.cron({
         minute: "1",
-        hour: "6-18",
+        hour: "13-1",
         weekDay: "*",
       }),
       targets: [new eventTargets.LambdaFunction(scheduledFn)],
